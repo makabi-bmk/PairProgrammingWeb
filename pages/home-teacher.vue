cda62abb009badc4d04c2097a172635bb4cedc87
makabi-bmk <nojiko0101@gmail.com>
2022-08-27 18:24:22 +0900

    タブはやした

--- pages/home-teacher.vue
+++ pages/home-teacher.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <h1>User Profile</h1>
-    <p v-if="User != null">Your e-mail is {{ User.email }}</p>
+    <div>
+    <!-- <h1>User Profile</h1> -->
+    <!-- <p v-if="User != null">Your e-mail is {{ User.email }}</p>
     <button @click="logout">Logout</button>
-    <p>ID = {{ message }}</p>
+    <p>ID = {{ message }}</p> -->
+<v-tabs>
+  <v-tab href="#tab-1">One</v-tab>
+  <v-tab href="#tab-2">Two</v-tab>
+  <v-tab href="#tab-3">Three</v-tab>
+
+  <v-tab-item value="tab-1">
+    Tab 1 Content
+  </v-tab-item>
+  <v-tab-item value="tab-2">
+    Tab 2 Content
+  </v-tab-item>
+  <v-tab-item value="tab-3">
+    Tab 3 Content
+  </v-tab-item>
+</v-tabs>
   </div>
 </template>
 
@@ -14,6 +29,7 @@ export default {
 //   middleware: "auth",
   data() {
     return {
+      tab: 1,
       User: this.$store.state.authUser,
       message: this.$store.state.classID
     }
@@ -28,3 +44,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+div {
+  width: 100%;
+}
+v-tabs {
+  width: 100%;
+}
+v-tab {
+  width: 50px;
+}
+</style>
