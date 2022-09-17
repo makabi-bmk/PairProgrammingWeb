891fb955b6282f9ca99aece55c3d868edb88253a
makabi-bmk <nojiko0101@gmail.com>
2022-09-18 06:41:23 +0900

    いらないファイル消してoriginをallにした

--- api/index.js
+++ api/index.js
@@ -7,7 +7,8 @@ var pairDict = {};
 // const cors = require('cors')
 const io = require('socket.io')(server, {
     cors: {
-        origin: "http://ict-edu.okinawa-ct.ac.jp",
+        // origin: "http://ict-edu.okinawa-ct.ac.jp",
+        origin: '*',
         methods: ["GET", "POST"],
         allowHeaders: [""]
         // credentials: rue
