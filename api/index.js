762c9ae0ed47d2f8544948d694252ca9a6ea1c98
makabi-bmk <nojiko0101@gmail.com>
2023-01-23 13:50:39 +0900

    url更新するの忘れてた

--- api/index.js
+++ api/index.js
@@ -20,8 +20,8 @@ var rejectHelpCount = 0;
 // const cors = require('cors')
 const io = require('socket.io')(server, {
     cors: {
-        // origin: "http://ict-edu.okinawa-ct.ac.jp",
-        origin: "http://localhost:3000",
+        origin: "http://ict-edu.okinawa-ct.ac.jp",
+        // origin: "http://localhost:3000",
         methods: ["GET", "POST"],
         allowHeaders: [""]
         // credentials: rue
